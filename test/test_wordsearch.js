12e7dfba810d9deee831da23216b0e67567d30a3
Ahmed Alani <s3leha@gmail.com>
2020-06-21 06:56:50 -0700

    wordsearch.js passes all test including vertical search and eympty array

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 const chai = require('chai');
 const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +21,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +35,25 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+  it("should return true if the word is present vertically", function () {
+    const result = wordSearch([
+      ['S', 'z', 'Z', 'Z', 'z', 'E', 'L', 'D'],
+      ['E', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['L', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  it("should return false if passed in empty array", function () {
+    const result = wordSearch([], 'SEINFELD')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+/* eslint-disable */
+const transpose = function (matrix) {
+  let output = [];
+  matrix.forEach((arr) => {
+    arr.forEach((e, i) => {
+      if (output[i]) {
+        output[i].push(e)
+      } else {
+        output.push([e]);
+      }
+    });
+  });
+  return output;
+};
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''))
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true
+  }
+  return false;
 }
 
 module.exports = wordSearch
